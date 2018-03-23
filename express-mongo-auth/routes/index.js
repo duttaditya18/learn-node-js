bb23ee19f09bbc02fd8cff7f7fc8709ed91e669d
Aditya Dutt <duttaditya18@gmail.com>
2018-03-23 19:38:47 +0530

    got it

--- express-mongo-auth/routes/index.js
+++ express-mongo-auth/routes/index.js
@@ -16,4 +16,14 @@ router.get('/contact', function(req, res, next) {
   return res.render('contact', { title: 'Contact' });
 });
 
+// GET /register
+router.get('/register', function(req, res, next) {
+  return res.send("Register!");
+});
+
+// GET /register
+router.post('/register', function(req, res, next) {
+  return res.send("User Created!");
+});
+
 module.exports = router;
