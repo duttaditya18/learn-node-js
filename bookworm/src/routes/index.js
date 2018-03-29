bbff660cceaae3a0456e5d00111d20aa579200f4
Aditya Dutt <duttaditya18@gmail.com>
2018-03-29 16:43:12 +0530

    Sessions and some bullshit.

--- bookworm/src/routes/index.js
+++ bookworm/src/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var user = require('../models/user');
+var mid = require('../middleware/index');
 
 // GET /
 router.get('/', function(req, res, next) {
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
+router.get('/about', mid.requiresLogin, function(req, res, next) {
   return res.render('about', { title: 'About' });
 });
 
@@ -32,7 +33,7 @@ router.get('/logout', function(req, res, next) {
 });
 
 // GET /login
-router.get('/login', function(req, res, next) {
+router.get('/login', mid.loggedOut, function(req, res, next) {
   return res.render('login', { title: 'Login'});
 });
 
@@ -57,12 +58,7 @@ router.post('/login', function(req, res, next) {
 });
 
 // GET /profile
-router.get('/profile', function(req, res, next) {
-  if(!req.session.userId) {
-    var err = new Error('You are not authorized to view this page');
-    err.status = 403;
-    return next(err);
-  }
+router.get('/profile', mid.requiresLogin, function(req, res, next) {
   user.findById(req.session.userId).exec(function(error, user) {
     if(error) {
       return next(error);
@@ -74,7 +70,7 @@ router.get('/profile', function(req, res, next) {
 });
 
 // GET /register
-router.get('/register', function(req, res, next) {
+router.get('/register', mid.loggedOut, function(req, res, next) {
   return res.render('register', { title: 'Sign Up' });
 });
 
