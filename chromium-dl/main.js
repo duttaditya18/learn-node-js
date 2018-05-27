108e75ca850dce3ef3651974112871563aee006b
Aditya Dutt <duttaditya18@gmail.com>
2018-05-27 23:26:06 +0530

    stuff

--- chromium-dl/main.js
+++ chromium-dl/main.js
@@ -1,4 +1,19 @@
 const https = require('https');
+const fs = require('fs');
+const anchorme = require("anchorme")
+
+var downloadFile = (url, cb) => {
+    var file = fs.createWriteStream('chromium-sync.exe');
+    var request = https.get(url, (response) => {
+        response.pipe(file);
+        file.on('finish', () => {
+            file.close(cb);
+        });
+    }).on('error', (err) => {
+        fs.unlink(dest);
+        console.log("Error: " + err.message);
+    });
+};
 
 const options = {
     host: 'api.github.com',
@@ -15,10 +30,12 @@ https.get(options, (resp) => {
 
     resp.on('end', () => {
         const url = JSON.parse(data).assets[0].browser_download_url;
-        // downloadFile(url);
+        downloadFile(url, () => {
+            
+        });
         console.log(url);
     });
- 
+
 }).on("error", (err) => {
     console.log("Error: " + err.message);
 });
