06c16fe89f5aac0e19e40c959d6d75fb1066cd68
Aditya Dutt <duttaditya18@gmail.com>
2018-05-28 15:48:24 +0530

    s

--- chromium-dl/main.js
+++ chromium-dl/main.js
@@ -5,6 +5,10 @@ const dl = require('download-file-with-progressbar');
 const chalk = require('chalk');
 var progress = require('progress');
 
+var handleErrors = (err) => {
+
+}
+
 var redirectLinkFinder = (url) => {
     process.stdout.write(chalk.keyword('cyan')('Obtaining URL.'));
     https.get(url, (resp) => {
@@ -33,21 +37,22 @@ var downloadFile = (url) => {
         filename: 'chromium-sync.exe',
         dir: './',
         onDone: (info) => {
-            process.stdout.write('\r\x1b[K')
-            process.stdout.write(chalk.keyword('cyan')('Downloading Complete.'));
+            console.log('\n' + chalk.keyword('cyan')('Downloading Complete.'));
         },
         onError: (err) => {
             console.log('Error', err.message);
         },
         onProgress: (curr, total) => {
-            var bar = new progress('[:bar] ' + chalk.keyword('green')(':percent') + chalk.keyword('yellow')(`  ${(curr / 1048576).toFixed(2)} MB`) + chalk.keyword('gray')(` / ${(total / 1048576).toFixed(2)} MB`), {
-                complete: chalk.bgKeyword('white').keyword('black')(' '),
-                incomplete: chalk.keyword('gray')('-'),
-                width: 50,
-                total: total / 1048576
-            });
-            dank = curr - dank;
-            bar.tick(curr / 1048576);
+            if (curr !== total) {
+                var bar = new progress('[:bar] ' + chalk.keyword('green')(':percent') + chalk.keyword('yellow')(`  ${(curr / 1048576).toFixed(2)} MB`) + chalk.keyword('gray')(` / ${(total / 1048576).toFixed(2)} MB`), {
+                    complete: chalk.bgKeyword('white').keyword('black')(' '),
+                    incomplete: chalk.keyword('gray')('-'),
+                    width: 50,
+                    total: total / 1048576
+                });
+                dank = curr - dank;
+                bar.tick(curr / 1048576);
+            }
             // deletes the previous line
             // process.stdout.write('\r\x1b[K')
             // 1048576 is the number of bytes in a megabyte.
@@ -62,7 +67,7 @@ process.stdout.write(chalk.keyword('cyan')('Finding Package.'));
 
 var options = {
     host: 'api.github.com',
-    path: '/repos/henrypp/chromium/releases/latest',
+    path: '/repos/duttaditya18/love-calc/releases/latest',
     headers: { 'User-Agent': 'Mozilla/5.0' }
 };
 
@@ -74,7 +79,7 @@ https.get(options, (resp) => {
     });
 
     resp.on('end', () => {
-        var url = JSON.parse(data).assets[0].browser_download_url;
+        var url = JSON.parse(data).assets[1].browser_download_url;
         process.stdout.write('\r\x1b[K')
         process.stdout.write(chalk.keyword('cyan')(`Package Found`) + ' : ' + chalk.keyword('gray')(url) + '\n');
         redirectLinkFinder(url);
