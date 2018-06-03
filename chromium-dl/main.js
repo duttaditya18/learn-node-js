3ecc702dfea7ee1edf94cfbd0f2f63fbef3c0e83
Aditya Dutt <duttaditya18@gmail.com>
2018-06-03 17:50:32 +0530

    jkl

--- chromium-dl/main.js
+++ chromium-dl/main.js
@@ -64,6 +64,7 @@ var downloadFile = (url) => {
 };
 
 var type = 1;
+
 if(process.argv[2] === "--old" || process.argv[2] === "-o") {
     type = 0;
 } else if(process.argv[2] === "--help" || process.argv[2] === "-h" || process.argv[2] !== undefined) {
