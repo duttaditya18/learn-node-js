e8d35f39da9fa318f1f46c594f922ae2839c3db3
Aditya Dutt <duttaditya18@gmail.com>
2018-11-12 18:45:58 +0530

    Added API for registration

--- devconnector/server.js
+++ devconnector/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -20,7 +25,7 @@ app.get("/", (req, res) => res.send("Hello"));
 
 // Use Routes
 app.use("/api/users", users);
-app.use("/api/porfile", profile);
+app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
